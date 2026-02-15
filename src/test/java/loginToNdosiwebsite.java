bebc97c3a6af5d6faf7debb1fcd9beb72dcb241a
melokuhlemazii <melomazibuko8@gmail.com>
2026-02-15 18:05:20 +0200

    first commit

---
+++ src/test/java/loginToNdosiwebsite.java
@@ -0,0 +1,38 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class loginToNdosiwebsite {
+    WebDriver driver;
+
+    @Test
+    public void loginWithValidDetails(){
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://ndosisimplifiedautomation.vercel.app/#overview");
+        driver.findElement(By.xpath("//*[@id=\"app-root\"]/nav/div[1]/div[3]/button/span[2]")).click();
+        driver.findElement(By.id("login-email")).sendKeys("melomazibuko8@gmail.com");
+        driver.findElement(By.id("login-password")).sendKeys("Mwelase@1031");
+        driver.findElement(By.id("login-submit")).click();
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        By dashboardElement = By.xpath("//*[@id=\"app-main-content\"]/section");
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(dashboardElement));
+
+        boolean isDashboardVisible = driver.findElement(dashboardElement).isDisplayed();
+        Assert.assertTrue(isDashboardVisible, "Login failed - Dashboard not visible");
+
+        driver.quit();
+
+
+    }
+}
